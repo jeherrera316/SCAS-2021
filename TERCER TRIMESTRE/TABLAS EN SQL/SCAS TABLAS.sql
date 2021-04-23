536eca1f6a9f44e98bc2a4453b7e9f8e8df2f013
johanherrera02 <68306701+johanherrera02@users.noreply.github.com>
2021-04-22 21:49:30 -0500

    actualización 2

--- TERCER TRIMESTRE/TABLAS EN SQL/SCAS TABLAS.sql
+++ TERCER TRIMESTRE/TABLAS EN SQL/SCAS TABLAS.sql
@@ -1,20 +1,19 @@
 create database SCAS;
 use SCAS;
 
-create table if not exists TipoDocumento
+create table if not exists tipodocumento
 (
-
-Id_tipodocumento int (10) primary key unique,
+id_tipodocumento int (10) primary key unique,
 nombre varchar (20)
 
 );
 
 
-create table if not exists Personas
+create table if not exists personas
 (
 id_persona int (10) primary key auto_increment,
 id_tipodocumento int (10) not null,
-Numerodocumento int (15) unique not null,
+numerodocumento int (15) unique not null,
 nombres varchar (50),
 apellidos varchar (50),
 telefono int (20),
@@ -27,150 +26,151 @@ references tipoDocumento (id_tipodocumento)
 );
 
 
-create table if not exists TipoVehiculo
+create table if not exists tipovehiculo
 (
-Id_tipovehiculo int (10) primary key auto_increment,
+id_tipovehiculo int (10) primary key auto_increment,
 tipo text (20) not null
 );
 
 
-create table if not exists MarcaVehiculo
+create table if not exists marcavehiculo
 (
-Id_marcavehiculo int (10) primary key auto_increment,
+id_marcavehiculo int (10) primary key auto_increment,
 marca text (30) not null
 );
 
 
-create table if not exists Vehiculo
+create table if not exists opciones
 (
-Id_vehiculo int (10) primary key auto_increment,
-Id_tipovehiculo int (10) not null,
-Id_marcavehiculo int (10) not null,
+id_cargoempleado int (10) primary key auto_increment,
+opcion text (20) not null
+);
+
+
+create table if not exists vehiculo
+(
+id_vehiculo int (10) primary key auto_increment,
+id_tipovehiculo int (10) not null,
+id_marcavehiculo int (10) not null,
 color text (15),
 placa varchar(10) not null unique,
 soat text (15) not null,
 tecnomecanica text (15) not null,
 foto text (15),
 
-constraint fk_MarcaVehiculo_Vehiculo
-foreign key (Id_marcavehiculo)
-references MarcaVehiculo (Id_marcavehiculo),
+constraint fk_marcaVehiculo_vehiculo
+foreign key (id_marcavehiculo)
+references marcaVehiculo (id_marcavehiculo),
 
-constraint fk_TipoVehiculo_Vehiculo
-foreign key (Id_tipovehiculo)
-references TipoVehiculo (Id_tipovehiculo)
+constraint fk_tipovehiculo_vehiculo
+foreign key (id_tipovehiculo)
+references tipovehiculo (id_tipovehiculo)
 );
 
 
-create table if not exists CargoEmpleado
+create table if not exists cargoempleado
 (
-Id_cargoempleado int (10) primary key auto_increment,
+id_cargoempleado int (10) primary key auto_increment,
 cargo text (20) not null
 );
 
 
-create table if not exists Empleado
+create table if not exists empleado
 (
-Id_empleado int (10) primary key auto_increment,
-Id_cargoempleado int (10) not null,
-Id_persona int (10) not null,
+id_empleado int (10) primary key auto_increment,
+id_cargoempleado int (10) not null,
+id_usuario int (10) not null,
 
-constraint fk_CargoEmpleado_Empleado
-foreign key (Id_cargoempleado)
-references CargoEmpleado (Id_cargoempleado),
+constraint fk_cargoempleado_empleado
+foreign key (id_cargoempleado)
+references cargoempleado (id_cargoempleado),
 
-constraint fk_Personas_Empleado
-foreign key (Id_persona)
-references Personas (Id_persona)
+constraint fk_usuario_empleado
+foreign key (id_usuario)
+references usuario (id_usuario)
 
 );
 
 
-create table if not exists Usuario
+create table if not exists usuario
 (
-Id_usuario int (10) primary key auto_increment,
-Id_persona int (10) not null,
+id_usuario int (10) primary key auto_increment,
+id_persona int (10) not null,
 nombre_user varchar (20) not null,
 password_user varchar (20) not null,
 
-constraint fk_Personas_Usuario
-foreign key (Id_persona)
-references Personas (Id_persona)
+constraint fk_personas_usuario
+foreign key (id_persona)
+references personas (id_persona)
 );
 
 
-create table if not exists Solicitud
+create table if not exists solicitud
 (
-Id_solicitud int (10) primary key auto_increment,
-Id_usuario int (10) not null,
-Id_vehiculo int (10) not null,
+id_solicitud int (10) primary key auto_increment,
+id_usuario int (10) not null,
+id_vehiculo int (10) not null,
 
-constraint fk_Usuario_Solicitud
-foreign key (Id_usuario)
-references Usuario (Id_usuario),
+constraint fk_usuario_solicitud
+foreign key (id_usuario)
+references usuario (id_usuario),
 
-constraint fk_Vehiculo_Solicitud
-foreign key (Id_vehiculo)
-references Vehiculo (Id_vehiculo)
+constraint fk_vehiculo_solicitud
+foreign key (id_vehiculo)
+references vehiculo (id_vehiculo)
 
 );
 
-
-create table if not exists FranjaHoraria
+create table if not exists franjahoraria
 (
-Id_franjahoraria int (10) primary key auto_increment,
-horaentrada datetime null,
-horasalida datetime null
+id_franjahoraria int (10) primary key auto_increment,
+franja time not null
 );
 
-
-create table if not exists CupoAsignado
+create table if not exists cupoasignado
 (
-Id_cupoasignado int (10) primary key auto_increment,
+id_cupoasignado int (10) primary key auto_increment,
 numeroespacio int (10) not null,
-Id_franjahoraria int (10) not null,
+id_franjahoraria int (10) not null,
 fechaacceso date not null,
-Id_solicitud int (10) not null,
+id_solicitud int (10) not null,
 
-constraint fk_FranjaHoraria_CupoAsignado
-foreign key (Id_franjahoraria)
-references FranjaHoraria (Id_franjahoraria),
+constraint fk_franjahoraria_cupoAsignado
+foreign key (id_franjahoraria)
+references franjaHoraria (id_franjahoraria),
 
-constraint fk_Solicitud_CupoAsignado
-foreign key (Id_solicitud)
-references Solicitud (Id_solicitud)
+constraint fk_solicitud_cupoAsignado
+foreign key (id_solicitud)
+references solicitud (id_solicitud)
 
 );
 
 
-create table if not exists Registro
+create table if not exists registro
 (
-Id_registro int (10) primary key auto_increment unique,
+id_registro int (10) primary key auto_increment unique,
 fecha_registro date not null,
 horaingreso time not null,
 horasalida time,
-Id_usuario int (10) not null,
-Id_vehiculo int (10) not null,
-Id_cupoasignado int (10) not null,
+id_usuario int (10) not null,
+id_vehiculo int (10) not null,
+id_cupoasignado int (10) not null,
 
-constraint fk_Usuario_Registro
-foreign key (Id_usuario)
-references Usuario (Id_usuario),
+constraint fk_usuario_registro
+foreign key (id_usuario)
+references usuario (id_usuario),
 
-constraint fk_Vehiculo_Registro
-foreign key (Id_vehiculo)
-references Vehiculo (Id_vehiculo),
+constraint fk_vehiculo_registro
+foreign key (id_vehiculo)
+references vehiculo (id_vehiculo),
 
-constraint fk_CupoAsignado_Registro
-foreign key (Id_cupoasignado)
-references CupoAsignado (Id_cupoasignado)
+constraint fk_cupoAsignado_registro
+foreign key (id_cupoasignado)
+references cupoAsignado (id_cupoasignado)
 
 );
 
 -- Proyecto formativo - Entrega 1.
-
-insert into personas (Id_persona, Id_tipodocumento, Numerodocumento, nombres, apellidos, telefono, correo)
-values (1,1234567801,'Luis Alberto','Flores Ramirez',3121234561,'lflores@gmail.com');
 
 insert into tipodocumento (Id_tipodocumento, nombre)
 values (1,'Cedula de Ciudadania');
@@ -218,8 +218,31 @@ values	(1,'suzuki'),
         (24,'canyon'),
         (25,'optimus');
         
-insert into personas ( Id_tipodocumento, Numerodocumento, nombres, apellidos, telefono, correo)
-values (1,1020433218,'Maicol','Cubillos',3174302138,'maicol@gmail.com');
+insert into personas(id_persona,id_tipodocumento, numerodocumento, nombres, apellidos, telefono, correo)
+values (1,1,6951122423,'juan josŽ','berrocal herrero',7516812532,'jose@gmail.com');
+
+insert into personas(id_persona,id_tipodocumento, numerodocumento, nombres, apellidos, telefono, correo)
+values 
+(2,1,7813412410,'miguel angel','abril i–igo',6838781709,'miguel@gmail.com'),
+(3,1,6565420984,'juan sebastian','gamero esquinas',7536115521,'juan@gmail.com'),
+(4,1,6842444213,'samuel david','mora salmeron',8297249152,'samul@gmail.com'),
+(5,1,6193257220,'juan david','barrionuevo serna',8650728274,'david@gmail.com'),
+(6,2,8527279962,'juan pablo','peralta del rosario',6942481400,'pablo@gmail.com'),
+(7,2,6871918110,'juan diego','huerta novas',800319208,'diego@gmail.com'),
+(8,2,7190549934,'angel david','cubillo fabra',8662018114,'angel@gmail.com'),
+(9,1,7506192909,'juan esteban', 'casanova vadillo',8797470165,'esteban@gmail.com'),
+(1,1,679376654,'andres felipe','insua aguilera',6346106865,'felipe@gmail.com'),
+(11,1,7245254625,'maria jose','solsona rial',6646113781,'maria@gmail.com'),
+(12,2,6510797613,'maria','cuadrado duro',6724794721,'mariac@gmail.com'),
+(13,1,660855102,'maria teresa','monreal sanz',8829286670,'teresa@gmail.com'),
+(14,1,783468518,'juan antonio ','mangas cuellar',6529995066,'antonio@gmail.com'),
+(15,1,1453297227,'adrian ','pelayo grande',8154848652,'adrianc@gmail.com'),
+(16,1,6166775382,'jose ignacio','lemos arrizabalaga',8617339775,'ignacio@gmail.com'),
+(17,2,818891718,'antonio ','canovas giron',6991046203,'antonioc@gmail.com'),
+(18,1,785189642,'jose antonio', 'suero mateo',8692824221,'antonio@gmail.com'),
+(19,1,7510204092,'andrea', 'seijas naranjo',8268361500,'andreac@gmail.com');
+
+
 
 -- Consultas
 
