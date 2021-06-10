0f3d42961a90bcd6467c35b7de851d68a48b018f
johanherrera02 <68306701+johanherrera02@users.noreply.github.com>
2021-06-09 20:26:11 -0500

    actualizacion junio

--- Bases de Datos/SCAS TABLAS.sql
+++ Bases de Datos/SCAS TABLAS.sql
@@ -254,6 +254,7 @@ values (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),
 		(11),(12),(13),(14),(15),(16),(17),(18),(19),(20);               
 
 -- Orden de importar tablas: usuario, vehiculo, franjahoraria, empleado, solicitud, cupoasignado, registro
+-- actualizar usuarios 
 
 		
 
