4e4ac147e67ebe76536f24b0cb47c1d2ff1bd0ec
Edna Chaguala <ednachaguala518@gmail.com>
2021-06-28 21:43:37 -0500

    prueba 2

--- TERCER TRIMESTRE/Bases de Datos/SCAS TABLAS.sql
+++ TERCER TRIMESTRE/Bases de Datos/SCAS TABLAS.sql
@@ -256,6 +256,3 @@ values (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),
 -- Orden de importar tablas: usuario, vehiculo, franjahoraria, empleado, solicitud, cupoasignado, registro
 -- actualizar usuarios 
 
-		
-
-
